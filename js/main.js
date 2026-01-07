73b364b97c8a8db87e8348153c27869c5076a777
Валерия Зайкина <vchxve@mail.ru>
2026-01-08 01:25:39 +0500

    Fix code style issues

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-import './util.js';
 import './form.js';
 import './effects.js';
 import { getData } from './api.js';
 import { renderPhotos } from './render-pictures.js';
 import { initFilters } from './filters.js';
+
+const DATA_ERROR_Z_INDEX = 1000;
+const DATA_ERROR_REMOVE_DELAY = 5000;
 
 getData()
   .then((photos) => {
@@ -20,14 +22,15 @@ getData()
     errorBlock.style.left = '0';
     errorBlock.style.right = '0';
     errorBlock.style.top = '0';
-    errorBlock.style.zIndex = '1000';
+    errorBlock.style.zIndex = `${DATA_ERROR_Z_INDEX}`;
     errorBlock.style.padding = '10px 20px';
     errorBlock.style.textAlign = 'center';
     errorBlock.style.backgroundColor = '#ff4e4e';
     errorBlock.style.color = '#ffffff';
+
     document.body.append(errorBlock);
 
     setTimeout(() => {
       errorBlock.remove();
-    }, 5000);
+    }, DATA_ERROR_REMOVE_DELAY);
   });
